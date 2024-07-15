db7c348e39410a0309db0533d2d91882de8d4c98
ckckckcz <rriovld@gmail.com>
2024-07-15 18:23:24 +0700

    update: more information at jumbotron 1

--- resources/views/pages/course/roadmap/kelas/belajar-bahasa-pemrograman-python.blade.php
+++ resources/views/pages/course/roadmap/kelas/belajar-bahasa-pemrograman-python.blade.php
@@ -56,8 +56,50 @@
     <!-- Navbar Selesai -->
     <!-- Jumbotron 1 Mulai -->
     <section class="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')] py-8 lg:py-16">
-      <div class="px-4 mx-auto max-w-screen-xl text-left z-10 relative">
-        <h1 class="font-extrabold tracking-tight leading-none text-gray-900 dark:text-white justify-center text-center jumbs1">Belajar Bahasa Pemrograman Python</h1>   
+      <div class="px-4 mx-auto max-w-screen-xl text-left z-10 relative"> 
+        <div class="grid grid-row text-center items-center relative jumbs1">
+          <h1 class="font-extrabold tracking-tight leading-none text-gray-900 dark:text-white justify-center text-center mb-10">Belajar Bahasa Pemrograman Python</h1>  
+          <div class="information flex flex-row justify-center items-center lg:gap-8 sm:gap-4 gap-4 lg:mt-5 mt-5 sm:hidden lg:flex hidden">
+            <div class="release flex flex-row justify-center items-center gap-2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5">
+                <path class="dark:fill-white" fill="rgb(55 65 81)" d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z"/>
+              </svg>
+              <h1 class="font-semibold lg:text-md sm:text-sm text-sm dark:text-white text-gray-900">Kelas dirilis sejak Juli 2024</h1>
+            </div>
+            <div class="release flex flex-row justify-center items-center gap-2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5">
+                <path class="dark:fill-white" fill="rgb(55 65 81)" d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/>
+              </svg>
+              <h1 class="font-semibold lg:text-md sm:text-sm text-sm dark:text-white text-gray-900">39 Menit Belajar</h1>
+            </div>
+          </div>
+          <div class="more-information flex flex-row justify-center items-center lg:mt-5 mt-10 sm:mt-2 sm:hidden lg:flex hidden" style="gap: 40px">
+            <div class="release flex flex-col items-center text-center gap-2">
+              <h1 class="font-semibold lg:text-md sm:text-sm text-lg dark:text-white text-gray-900">Pelajar Terdaftar</h1>
+              <p class="font-semibold lg:text-md sm:text-sm text-lg dark:text-white text-gray-900">400</p>
+            </div>
+            <div class="release flex flex-col items-center text-center gap-2">
+              <h1 class="font-semibold lg:text-md sm:text-sm text-lg dark:text-white text-gray-900">Level</h1>
+              <img src="{{asset('./images/dasar.svg')}}" class="w-6 h-6" alt="">
+            </div>
+            <div class="release flex flex-col items-center text-center gap-2" style="margin-top:7px">
+              <h1 class="font-semibold lg:text-md sm:text-sm text-lg dark:text-white text-gray-900">Sertifikat</h1>
+              <div class="checklist-circle">
+                <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_981_529)">
+                  <path d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24Z" fill="#E8EFFF"/>
+                  <path d="M7 12L10.1579 15L17 9" stroke="#146FFE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  </g>
+                  <defs>
+                  <clipPath id="clip0_981_529">
+                  <rect width="24" height="24" fill="white"/>
+                  </clipPath>
+                  </defs>
+                </svg>                            
+              </div>
+            </div>           
+          </div>        
+        </div>
         <div class="flex flex-col lg:flex-row gap-6 p-6">
           <!-- Image and Description Section -->
           <div class="lg:w-1/2">
@@ -92,7 +134,7 @@
                     </li>
                 </ul>
             </div>
-            <button type="submit" class="w-full mt-6 py-6 font-medium text-white bg-blue-700 rounded" style="border-radius: 0px 0px 20px 20px; font-size:25px">Gabung Sekarang</button>
+            <button type="submit" class="w-full mt-6 py-6 font-medium text-white bg-blue-700 rounded" style="border-radius: 0px 0px 20px 20px; font-size:22px">Gabung Sekarang</button>
           </div>          
         </div>
       </div>
