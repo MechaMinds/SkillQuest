6f42bd6e5d404bef60b9678d115721d56eab8e75
ckckckcz <rriovld@gmail.com>
2024-08-29 15:29:52 +0700

    update: thumbnail course

--- resources/views/pages/roadmap/machine-learning.blade.php
+++ resources/views/pages/roadmap/machine-learning.blade.php
@@ -149,7 +149,7 @@
                                 </div>
                             </div>                          
                             <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" style="margin-bottom: 20px" data-tags="Semua Dasar">
-                                <img class="rounded-t-lg" src="{{asset('./images/thumbnail/thumbnail1.png')}}" alt="" />
+                                <img class="rounded-t-lg" src="{{asset('./images/thumbnail/thumbnail11.png')}}" alt="" />
                                 <div class="p-5">
                                     <h5 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-left">Belajar Bahasa Pemrograman Python</h5>
                                     <div class="mb-4 flex flex-wrap gap-4">
